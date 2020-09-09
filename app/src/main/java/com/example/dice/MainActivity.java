3668427a50c05813c986f38f078ce55a4db2fd5c
Garrett Powell <garettbpowell@gmail.com>
2020-09-09 17:17:43 -0500

    did some horrid things

--- app/src/main/java/com/example/dice/MainActivity.java
+++ app/src/main/java/com/example/dice/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.dice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer player;
     private ImageView imageViewDice;
     private Random rng = new Random();
     @Override
@@ -29,29 +32,93 @@ public class MainActivity extends AppCompatActivity {
 
     private void rollDice()
     {
-        int randomNumber = rng.nextInt(6) + 1;
+        int randomNumber = rng.nextInt(20) + 1;
+
+
 
         imageViewDice = findViewById(R.id.image_view_dice);
-
+        TextView words = findViewById(R.id.critical);
         switch(randomNumber)
         {
             case 1:
                 imageViewDice.setImageResource(R.drawable.dice1);
+                words.setText("Critical Miss!");
                 break;
             case 2:
                 imageViewDice.setImageResource(R.drawable.dice2);
+                words.setText("");
                 break;
             case 3:
                 imageViewDice.setImageResource(R.drawable.dice3);
+                words.setText("");
                 break;
             case 4:
                 imageViewDice.setImageResource(R.drawable.dice4);
+                words.setText("");
                 break;
             case 5:
                 imageViewDice.setImageResource(R.drawable.dice5);
+                words.setText("");
                 break;
             case 6:
                 imageViewDice.setImageResource(R.drawable.dice6);
+                words.setText("");
+                break;
+            case 7:
+                words.setText("");
+                imageViewDice.setImageResource(R.drawable.s7);
+                break;
+            case 8:
+                imageViewDice.setImageResource(R.drawable.s8);
+                words.setText("");
+                break;
+            case 9:
+                imageViewDice.setImageResource(R.drawable.s9);
+                words.setText("");
+                break;
+            case 10:
+                imageViewDice.setImageResource(R.drawable.s10);
+                words.setText("");
+                break;
+            case 11:
+                imageViewDice.setImageResource(R.drawable.s11);
+                words.setText("");
+                break;
+            case 12:
+                imageViewDice.setImageResource(R.drawable.s12);
+                words.setText("");
+                break;
+            case 13:
+                imageViewDice.setImageResource(R.drawable.s13);
+                words.setText("");
+                break;
+            case 14:
+                imageViewDice.setImageResource(R.drawable.s14);
+                words.setText("");
+                break;
+            case 15:
+                imageViewDice.setImageResource(R.drawable.s15);
+                words.setText("");
+                break;
+            case 16:
+                imageViewDice.setImageResource(R.drawable.s16);
+                words.setText("");
+                break;
+            case 17:
+                imageViewDice.setImageResource(R.drawable.s17);
+                words.setText("");
+                break;
+            case 18:
+                imageViewDice.setImageResource(R.drawable.s18);
+                words.setText("");
+                break;
+            case 19:
+                imageViewDice.setImageResource(R.drawable.s19);
+                words.setText("");
+                break;
+            case 20:
+                imageViewDice.setImageResource(R.drawable.s20);
+                words.setText("Critical Hit!");
                 break;
         }
     }
